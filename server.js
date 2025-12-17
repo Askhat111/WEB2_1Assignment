8441c529908f469bb926383b71b963a126cf292e
Askhat111 <240680@astanait.edu.kz>
2025-12-17 17:17:53 +0500

    server.js

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('.')); 
+app.use(express.static('.'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.post('/calculate-bmi', (req, res) => {
-    const { weight, height } = req.body;
-    
-    const bmi = weight / (height * height);
-    res.redirect(`/?bmi=${bmi.toFixed(2)}`);
+  const weight = parseFloat(req.body.weight);
+  const heightCm = parseFloat(req.body.height);
+  const height = heightCm / 100;     
+  const bmi = weight / (height * height);
+  res.redirect(`/?bmi=${bmi.toFixed(2)}`);
 });
 
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+  console.log('Server http://localhost:3000');
 });
